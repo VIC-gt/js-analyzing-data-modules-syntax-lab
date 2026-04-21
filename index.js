a1d3e2d6ecbdb9480b6d1ab90f02ee42391b5cc6
Victor Mwaura Mwangi <victor.mwangi6@student.moringaschool.com>
2026-04-21 16:02:13 +0300

    done again

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 function combineUsers(...arrays) {
-  // Merge all arrays using spread operator
   let mergedUsers = [].concat(...arrays);
-
-  // Get today's date
   let today = new Date().toISOString().split("T")[0];
-
-  // Return required object
   return {
     users: mergedUsers,
     merge_date: today
